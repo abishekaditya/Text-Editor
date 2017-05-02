b348224ca2e3ea58428ad854771f5d4e1192c3aa
Ruchir Bhardwaj <ruchir2307@gmail.com>
2017-05-03 00:46:20 +0530

    JavaScript for saving the file as plain text
    Add the id="inputTextToSave" in the textarea tag
    also try to include this js in the head tag

--- download.js
+++ download.js
@@ -16,12 +16,14 @@ function saveTextAsFile()
 
     downloadLink.download = fileNameToSaveAs;
 
+	//"You cant see me" <- yah yah
+	//name for the link, since this will be hidden any name can be used
     downloadLink.innerHTML = "My Hidden Link";
     
 
     window.URL = window.URL || window.webkitURL;
           
-
+	//create link object for the file
     downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
 
 
@@ -42,5 +44,5 @@ function destroyClickedElement(event)
 {
 // remove the link from the DOM
     document.body.removeChild(event.target);
-}
+
 
